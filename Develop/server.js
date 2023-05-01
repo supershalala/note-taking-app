0d0e63d25e212ed41b562bb2c3270eec3663a494
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-05-01 15:49:44 +1000

    bug fixed where previous notes were not displaying

--- Develop/server.js
+++ Develop/server.js
@@ -8,19 +8,19 @@ const PORT = process.env.PORT || 3000;
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Middleware to parse incoming request bodies
+app.use(express.json());
+
 // Route to serve notes.html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
-// Route to serve index.html for all other requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+
 
 // RETURN all saved notes in DB to notes html 
 app.get('/api/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+  fs.readFile(path.join(__dirname,'db', 'db.json'), 'utf8', (err, data) => {
     if (err) {
       console.error(err);
       res.status(500).json({ error: 'Failed to read the notes database' });
@@ -28,6 +28,11 @@ app.get('/api/notes', (req, res) => {
       res.json(JSON.parse(data));
     }
   });
+});
+
+// Route to serve index.html for all other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
