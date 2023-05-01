ce6e3890ca4426336c4d77da8e4d2a768733004d
Adam Shalala <adamshalala@Adams-Air.localdomain>
2023-05-01 15:27:45 +1000

    express setup and GET for index and notes hmtl files

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route to serve notes.html
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+// Route to serve index.html for all other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
